bb42c82dedbb505bd0e7ccaf4985a6c7a23067a8
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2024-06-06 11:56:57 -0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include <PZEM004Tv30.h>
+
+namespace {
+    PZEM004Tv30 pzem(Serial2, 16, 17);
+}
+
+void setup() {
+    Serial.begin(115200);
+
+}
+
+void loop() {
+    Serial.print("Custom Address:");
+    Serial.println(pzem.readAddress(), HEX);
+
+    // Read the data from the sensor
+    float voltage = pzem.voltage();
+    float current = pzem.current();
+    float power = pzem.power();
+    float energy = pzem.energy();
+    float frequency = pzem.frequency();
+    float pf = pzem.pf();
+
+    // Check if the data is valid
+    if(isnan(voltage)){
+        Serial.println("Error reading voltage");
+    } else if (isnan(current)) {
+        Serial.println("Error reading current");
+    } else if (isnan(power)) {
+        Serial.println("Error reading power");
+    } else if (isnan(energy)) {
+        Serial.println("Error reading energy");
+    } else if (isnan(frequency)) {
+        Serial.println("Error reading frequency");
+    } else if (isnan(pf)) {
+        Serial.println("Error reading power factor");
+    } else {
+
+        // Print the values to the Serial console
+        Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+        Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
+        Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
+        Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
+        Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
+        Serial.print("PF: ");           Serial.println(pf);
+
+    }
+
+    Serial.println();
+    delay(2000);
+}
